628fb5e3da1f93bf73f2a37d6b94b1aea00f6765
Temistokles <temistokles@gmail.com>
2012-02-18 00:59:20 +0100

    Moving of the tasks - fixed

--- app/models/task.rb
+++ app/models/task.rb
@@ -7,19 +7,21 @@ class Task < ActiveRecord::Base
   def move_task(direction = :up)
   
     #selection of the task which priority will be replaced
-    selectPriority = direction == :up ? "priority <= ?" : "priority >= ?"
-    order = direction == :up ? "priority" : "priority DESC"
+    selectPriority = direction == :up ? "priority < ?" : "priority > ?"
+    order = direction == :up ? "priority DESC" : "priority"
   
     Task.transaction do
       task_to_replace = Task.where(:user_id => user_id).where(selectPriority, priority).order(order).first
-      this_task = Task.where(:id => id).first
-      
-      old_priority = task_to_replace.priority
-      task_to_replace.priority = priority      
-      this_task.priority = old_priority
-      
-      this_task.save
-      task_to_replace.save
+      if task_to_replace
+        this_task = Task.where(:id => id).first
+        
+        old_priority = task_to_replace.priority
+        task_to_replace.priority = priority      
+        this_task.priority = old_priority
+        
+        this_task.save
+        task_to_replace.save
+      end  
     end       
   end
   
